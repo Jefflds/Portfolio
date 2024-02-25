0fb4c8ccc52a61863653c07bee390462e3da3e87
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-25 17:30:14 -0300

    tailwindcss SETUP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import Header from "./ui/partials/Header";
+import Home from "./pages/Home";
+import Footer from "./ui/partials/Footer";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
