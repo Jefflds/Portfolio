a6a4be374d961eb4768237d9db324796ebef1782
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-25 22:54:44 -0300

    Adicionando particulas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,29 @@
-import Header from "./ui/partials/Header";
-import Home from "./pages/Home";
-import Footer from "./ui/partials/Footer";
+// import Header from "./ui/partials/Header";
+// import Home from "./pages/Home";
+// import Footer from "./ui/partials/Footer";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Engine } from "@tsparticles/engine";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
+import { ISourceOptions } from "@tsparticles/engine";
 
 const App = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine: Engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <div>
+      {init && <Particles options={particlesOptions as ISourceOptions} />}
+      
+    </div>
   );
 };
 
