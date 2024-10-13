f4bf3e53436b6ed710a687fab0450f47b111e1db
jefflds <jeffersonlds3009@gmail.com>
2024-10-13 19:15:48 -0300

    refactor: remove server from repository

--- server/src/config/env/index.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export const EnvVariables = {
-    node_env: process.env.NODE_ENV,
-    frontEndIP: process.env.FRONT_END_IP,
-    Port: process.env.PORT,
-};
