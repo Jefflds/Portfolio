37ec1fcc18e0f1e2686f299bde7dec1f331abe1a
jefflds <jeffersonlds3009@gmail.com>
2024-10-13 22:58:23 +0000

    fix: edit vite config to docker work correctly

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      port: 5172,
+    },
+  },
 })
